aabdd5e1f1153c64fabae99f2c4b28f68a91d20b
Elie <elieelaaraj50@gmail.com>
2025-10-20 16:15:26 +0300

    Add CORS and bootstrap error handling

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.enableCors({
-		origin: '',
+		origin: process.env.ORIGIN!,
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials: true,
 	});
 
 	await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+	console.error('Error during app bootstrap:', err);
+	process.exit(1);
+});
